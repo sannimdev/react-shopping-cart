a199227768a9d0e4b9b3ffc0fc03455910416564
Hojoong Choi <inegg.apps@gmail.com>
2023-04-18 11:51:50 +0900

    페이지 목업 정도만

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import { Header } from "./components";
-import cartIcon from "./assets/svgs/cart.svg";
-import productThumbnail from "./assets/images/product.png";
+import { ProductList } from "./components/Product";
+
+//TODO: 지우기
+import { sampleProducts } from "./tododelete/sampledb";
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <section className="product-container">
-        <div>
-          <img src={productThumbnail} alt="PET보틀-정사각(420ml)" />
-          <div className="flex justify-between w-280 p-5">
-            <div className="product-info">
-              <span className="product-info__name">PET보틀-정사각(420ml)</span>
-              <span className="product-info__price">43,000원</span>
-            </div>
-            <img src={cartIcon} alt="장바구니" />
-          </div>
-        </div>
-      </section>
+      <ProductList products={sampleProducts} />
     </div>
   );
 }
