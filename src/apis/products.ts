967ee6cbf430ab747f587d306ec4485b862f4bd9
Hojoong Choi <inegg.apps@gmail.com>
2023-04-28 10:37:21 +0900

    refactor: react-query 페이징 로직 수정

--- src/apis/products.ts
+++ src/apis/products.ts
@@ -2,6 +2,9 @@ import { IProductResponse } from "../domain/shopping-cart/types";
 import fetcher from "../utils/fetcher";
 import { API_URL } from "./constants";
 
-export async function getProducts() {
-  return (await fetcher.get(API_URL.PRODUCTS)) as IProductResponse;
+export async function getProducts({ page = 1, unit = 20 }) {
+  const params = new URLSearchParams({ page: page.toString(), unit: unit.toString() });
+  const URL = `${API_URL.PRODUCTS}?${params.toString()}`;
+  const response = (await fetcher.get(URL)) as IProductResponse;
+  return response;
 }
