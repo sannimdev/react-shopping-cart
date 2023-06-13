5032836e1ca8aa60e6d43bd36acd9e019e48ee3d
Hojoong Choi <inegg.apps@gmail.com>
2023-06-13 09:11:51 +0900

    recoil 설치

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { Header } from "./components";
 import { CartContextProvider } from "./context/CartContext/CartContext";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 function App({ children }: PropsWithChildren) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <CartContextProvider>
-        <div className="App">
-          <Header />
-          {children}
-        </div>
-      </CartContextProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <CartContextProvider>
+          <div className="App">
+            <Header />
+            {children}
+          </div>
+        </CartContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
 
