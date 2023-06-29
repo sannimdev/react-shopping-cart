b051a47202e279ce7c24a566171229530fe9cdee
Hojoong Choi <inegg.apps@gmail.com>
2023-06-29 16:19:58 +0900

    feat: 무한스크롤 쿼리 진행 중...page param 어떻게 처리할까

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,19 +1,44 @@
 import { useNavigate } from "react-router-dom";
-import { requestAddItem } from "../apis";
+import { requestAddItem, requestProducts } from "../apis";
 import { IProduct } from "../domain/types";
 import { IResponseError } from "../domain/types/response";
 import useProductsQuery from "../queries/useProductsQuery";
 import { convertToViewError } from "./utils";
+import { useInfiniteQuery } from "react-query";
+import fetcher from "../utils/fetcher";
+import { useRef } from "react";
 
 const useProducts = () => {
-  const { data, status, error: queryError } = useProductsQuery();
+  // const { data, status, error: queryError } = useProductsQuery();
+
+  const pageRef = useRef();
+
+  const {
+    data,
+    status,
+    fetchNextPage,
+    error: queryError,
+  } = useInfiniteQuery(
+    "products",
+    async ({ pageParam = 0 }) => {
+      console.log(pageParam, "pageParam");
+      const res = await requestProducts({ page: pageParam + 1, unit: 12 });
+      return res.data;
+    },
+    {
+      getPreviousPageParam: (firstPage: any) => firstPage.previousId ?? 1,
+      getNextPageParam: (lastPage: any) => lastPage.nextId ?? 1,
+    }
+  );
 
   const error = convertToViewError(queryError as IResponseError);
 
-  const products = data?.products || [];
+  const products = data?.pages.reduce((result, current) => {
+    return [...result, ...current.products];
+  }, [] as IProduct[]);
 
   const navigate = useNavigate(); // TODO: 모달창으로 대체되면 삭제되어야 할 코드일 수도 있음
-  const handleAddToCart = async (product: IProduct, isRequiredMovePage = true) => {
+  const handleAddToCart = async (product: IProduct, isRequiredMovePage = false) => {
     if (!confirm(`${product.name}을 장바구니에 담으시겠습니까?`)) {
       return;
     }
@@ -34,6 +59,6 @@ const useProducts = () => {
     }
   };
 
-  return { status, error, products, handleAddToCart };
+  return { status, error, products, handleAddToCart, fetchNextPage };
 };
 export default useProducts;
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 import { ProductItem } from "../../components/ProductItem";
 import { useProducts } from "../../hooks";
+import { IProduct } from "../../domain/types";
 
 const template = (children: React.ReactNode) => <section className="product-container">{children}</section>;
 
 function Products() {
-  const { status, error, products, handleAddToCart } = useProducts();
+  const { ref, inView } = useInView();
+
+  const { status, error, products, handleAddToCart, fetchNextPage } = useProducts();
+
+  useEffect(() => {
+    if (inView) {
+      console.log("페이지 도달...");
+      fetchNextPage();
+    }
+  }, [inView]);
 
   if (status === "loading") {
     return template(<div>loading...</div>);
@@ -15,8 +26,17 @@ function Products() {
     return template(<div>{error.message}</div>);
   }
 
-  return template(
-    products?.map((product) => <ProductItem key={product.id} product={product} onAddInCart={handleAddToCart} />)
+  return (
+    <>
+      {template(
+        products?.map((product: IProduct) => (
+          <ProductItem key={product.id} product={product} onAddInCart={handleAddToCart} />
+        ))
+      )}
+      <p style={{ backgroundColor: "red" }} ref={ref}>
+        dflkasdjflsalkdfjlkdjflksj
+      </p>
+    </>
   );
 }
 
