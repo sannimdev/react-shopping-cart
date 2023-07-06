885bff5e172eab81c29e5bc632971c0707e5c6d6
Hojoong Choi <inegg.apps@gmail.com>
2023-07-06 14:27:17 +0900

    refactor: 장바구니

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,13 @@
 import React, { Suspense } from "react";
 import CartList from "./CartList";
 import CartTemplate from "./CartTemplate";
+import { Spinner } from "../../components/Spinner";
 
 function Cart() {
   return (
     <CartTemplate>
-      <Suspense fallback={<div>loading..</div>}>
+      {/* 장바구니 구현은 이정도까지만 하겠습니다... */}
+      <Suspense fallback={<Spinner />}>
         <CartList />
       </Suspense>
     </CartTemplate>
